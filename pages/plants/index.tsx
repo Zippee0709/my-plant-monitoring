9dea9819bfe029f94ecb49ac819e9ecd3be7cdef
Nirsu <melvyn.fontaine@epitech.eu>
2022-12-02 15:39:47 +0100

    [FEAT](plants): add search input

--- pages/plants/index.tsx
+++ pages/plants/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, Row, Spacer, Text } from '@nextui-org/react';
+import { Button, Container, Grid, Input, Row, Spacer, Text } from '@nextui-org/react';
 import PlantPreviewCard from '../../components/cards/PlantPreviewCard';
 import AddIcon from '../../components/icons/AddIcon';
 import UserNavbar from '../../components/navbars/UserNavbar';
@@ -23,7 +23,13 @@ const Plants = () => {
             <Text b size='$2xl' color='#224722'>
               All my plants
             </Text>
-            <Button auto icon={<AddIcon color='#FFFFFF' width={16} height={16} />} color='success' />
+            <Container display='flex' justify='flex-end' gap={0}>
+              <Input clearable bordered color='success' placeholder='Search' />
+              <Spacer x={0.5} />
+              <Button auto icon={<AddIcon color='#FFFFFF' width={16} height={16} />} color='success'>
+                Add a plant
+              </Button>
+            </Container>
           </Container>
           <Grid.Container gap={2} justify='flex-start'>
             <Grid xs={6} sm={3}>
