1fdb387f475cf408aa2aa978e91d00568333b819
Nirsu <melvyn.fontaine@epitech.eu>
2022-12-03 05:24:20 +0100

    [FEAT](navbar): add call to back to get user info

--- components/navbars/UserNavbar.tsx
+++ components/navbars/UserNavbar.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Link from 'next/link';
 import { Container, Row, Col, Navbar, Text, Avatar } from '@nextui-org/react';
 
 import PlantIcon from '../icons/PlantIcon';
 import BellIcon from '../icons/BellIcon';
 import styles from './Navbar.module.scss';
+import AuthContext from '../../contexts/auth.context';
+import { useShallowEffect } from '@mantine/hooks';
+import { RequestFailedResponseType } from '../../types/clientApi.types';
+import { UserType } from '../../types/user.types';
+import { useRouter } from 'next/router';
 
 const UserNavbar = () => {
+  const authContext = useContext(AuthContext);
+  const router = useRouter();
+
+  const [name, setName] = useState<string>('');
+
+  useShallowEffect(() => {
+    const updateUserInfos = async () => {
+      if (authContext) {
+        const rep = await authContext.GetCurrentUser();
+        if ((rep as RequestFailedResponseType).error) {
+          // TODO: repair this
+          // toast.error(t('errors.default'), {
+          //   position: 'top-right',
+          //   autoClose: 2500,
+          //   hideProgressBar: false,
+          //   closeOnClick: true,
+          //   pauseOnHover: false,
+          //   draggable: true,
+          //   progress: undefined,
+          //   theme: 'dark',
+          // });
+          router.push('/auth/signin');
+          return;
+        }
+        const user = rep as UserType;
+        authContext!.user = user;
+        setName(`${user.firstname} ${user.lastname}`);
+      }
+    };
+    updateUserInfos();
+  }, [authContext]);
+
   return (
     <Navbar variant='static' maxWidth='fluid' css={{ justifyContent: 'space-between' }}>
       <Navbar.Brand>
@@ -23,10 +60,10 @@ const UserNavbar = () => {
         </Link>
         <Container display='flex' direction='row' css={{ padding: 0 }}>
           <Row gap={1}>
-            <Avatar text='ZW' />
+            <Avatar text={name[0]} />
             <Col css={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
               <Text size={12} b css={{ lineHeight: '16px' }}>
-                Zhiwen Wang
+                {name}
               </Text>
               <Text size={12} css={{ lineHeight: '16px' }}>
                 User
